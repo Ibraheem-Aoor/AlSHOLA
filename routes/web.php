97a28ab2bb2b73feb0bb966c78d4304a97069292
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-04-18 18:06:52 +0300

    setting the front template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.home');
 });
+Route::get('/about', function () {
+    return view('front.about');
+})->name('about');
+
+Route::get('/category', function () {
+    return view('front.category');
+})->name('categories');
+
+Route::get('/contact', function () {
+    return view('front.contact');
+})->name('contact');
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
