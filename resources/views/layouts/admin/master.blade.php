72ae5f423e1013b0727e713756d786d9e99dd179
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-04-21 05:04:37 +0300

    fixing layout on testing

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -7,6 +7,7 @@
 <!--<![endif]-->
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Ela Admin - HTML5 Admin Template</title>
