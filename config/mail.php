ae7483333b0e7bc83b0824d8e13d92ac3f595a29
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-05-09 03:33:48 +0300

    fixing mails

--- config/mail.php
+++ config/mail.php
@@ -77,6 +77,14 @@ return [
                 'log',
             ],
         ],
+
+        'stream' => [
+            'ssl' => [
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ]
     ],
 
     /*
