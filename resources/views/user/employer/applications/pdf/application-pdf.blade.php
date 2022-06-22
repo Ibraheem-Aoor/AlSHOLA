0952e1aca5a90b0c6a631fb02bd7ff215c7971c4
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-22 07:57:13 +0300

    fixing application pdf

--- resources/views/user/employer/applications/pdf/application-pdf.blade.php
+++ resources/views/user/employer/applications/pdf/application-pdf.blade.php
@@ -66,7 +66,7 @@
         </p>
     </div>
     <br>
-    <img src="{{asset('storage/uploads/applications/2/attachments/ali-jassem.png')}}" width="200" height="200"
+    <img src="{{asset('storage/uploads/applications/'.$application->id.'/attachments'.'/'.$application->employer_photo)}}" width="200" height="200"
         style="margin-left:70%;border: 1px solid black;">
     <table style="margin-top: -150px">
         <tr>
