85d7bec5c39c60d9ca67e2a183bcbdde6750e906
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-06-23 17:08:52 +0300

    edit

--- app/Http/Controllers/User/Employer/Applications/EmployerApplicationsController.php
+++ app/Http/Controllers/User/Employer/Applications/EmployerApplicationsController.php
@@ -31,7 +31,7 @@ class EmployerApplicationsController extends Controller
         $jobIds = Job::whereBelongsTo(Auth::user())->pluck('id');
         $applications = Application::whereIn('job_id' , $jobIds)
                         ->where('forwarded' , true)
-                        ->with(['job:id,post_number' , 'title' ,'user:id,name,type' , 'Job' , 'mainStatus' , 'subStatus'])->with('job.subJobs.title.sector')->withCount('attachments')
+                        ->with(['job:id,post_number' , 'title' ,'user:id,name,type' , 'Job' , 'mainStatus' , 'job.subStatus'])->with('job.subJobs.title.sector')->withCount('attachments')
                         ->simplePaginate(15);
         return view('user.employer.applications.all-applications' , compact('applications'));
     }//end mthod
