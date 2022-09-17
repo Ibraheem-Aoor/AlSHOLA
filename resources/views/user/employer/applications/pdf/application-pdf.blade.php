6ea34d4cdd99cc2214a93f6b165c2b3b75a56481
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-09-17 07:39:05 +0300

    up

--- resources/views/user/employer/applications/pdf/application-pdf.blade.php
+++ resources/views/user/employer/applications/pdf/application-pdf.blade.php
@@ -112,7 +112,7 @@
     @php
         $photo = $application->attachments->where('type', 'Personal Photo')->first()->name;
     @endphp
-    <img src="{{ Storage::url('public/uploads/applications' . '/' . $application->id . '/attachments' . '/' . $photo) }}"
+    <img src="{{ Storage::url('public/uploads/applications/' . $application->id . '/attachments' . '/' . $photo) }}"
         width="200" height="200" style="margin-left:70%;border: 1px solid black;">
     <br>
     <table style="margin-top: -150px">
